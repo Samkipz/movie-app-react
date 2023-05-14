4210cc2def13b86079c97d32435161b0edc1ea88
Samuel Ndiema <ndiemasam@gmail.com>
2023-05-15 00:45:00 +0300

    designed a basic footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import './Footer.scss'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <p>copyright &copy;2023 (Designed By - <a>Sam Ndiema</a>)</p>
+    </div>
   )
 }
 
