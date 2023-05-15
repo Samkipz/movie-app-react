bfd78a10286b37edbbe25087ef419a4b3467e471
Samuel Ndiema <ndiemasam@gmail.com>
2023-05-15 18:49:56 +0300

    fetched data from omdbAPI and output to console

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Home.scss'
+import {APIKey} from '../../common/Apis/MovieApiKey'
+import movieApi from '../../common/Apis/MovieApi'
 
 const Home = () => {
+  useEffect(()=>{
+    const movieText = "Avengers";
+    const fetchMovies = async () => {
+      const response = await movieApi.get(`?apikey=${APIKey}&s=${movieText}&type=movie`)
+      .catch((err)=>{
+        console.log("Error", err);
+      })
+      console.log("API results:", response)
+    };
+    fetchMovies()
+  }, []);
+  
   return (
     <div className='home'>
       <div className='movie-series'>
